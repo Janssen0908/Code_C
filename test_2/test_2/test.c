540155d6bd62ef8745c22269dab57f3cf0382245
Janssen000908 <2601566046@qq.com>
2023-01-21 21:29:47 +0800

    测试提交

---
+++ test_2/test_2/test.c
@@ -0,0 +1,31 @@
+#define   _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	//字符类型
+//	char ch = 'a';
+//	//整形
+//	int age = 20;
+//	//短整型
+//	short num = 10;
+//	//long
+//	//long long
+//	//单精度浮点型
+//	float weight = 55.5;
+//	//双精度浮点型
+//	double d = 0.9;
+//
+//	return 0;
+//}
+int main()
+{
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+
+	return 0;
+}
