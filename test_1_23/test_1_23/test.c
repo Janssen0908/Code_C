760dc5167c7c4c55f778afced9e37b39e9ca5aaf
Janssen000908 <2601566046@qq.com>
2023-01-23 22:41:56 +0800

    测试提交23

---
+++ test_1_23/test_1_23/test.c
@@ -0,0 +1,40 @@
+#define   _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+//int main()
+//{
+//	//字符数组-数组是一组数或者一组类型相同的元素
+///*	char arr[] = "hello";*///都是字符
+//	//char arr1[] = "abc";
+//	//char arr2[] = { 'a', 'b', 'c' };
+//
+//	////打印字符串
+//	//printf("%s\n", arr1);
+//	//printf("%s\n", arr2);
+//
+//	//int len = strlen("abc");
+//	//printf("%d\n", len);
+//	//printf("%d\n", strlen(arr1));
+//	//printf("%d\n", strlen(arr2));
+//
+//
+//
+//
+//
+//	return 0;
+//}
+int main()
+{
+	printf("c:\\test\\test.c\n");
+	printf("%c\n", '\'');
+	printf("%s\n", "\"");
+	printf("\a\n");
+	printf("?\n");
+	printf("\"\n");
+	printf("%d\n",strlen("c:\test\327\test.c"));
+
+
+
+	return 0;
+}
