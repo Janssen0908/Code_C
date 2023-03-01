0acb51ffe491762b05ee5eccc3fd41272afdd692
Janssen000908 <2601566046@qq.com>
2023-03-01 22:39:19 +0800

    测试提交1

---
+++ test_3_1/test_3_1/test.c
@@ -0,0 +1,113 @@
+#define   _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int cnt = 0;
+//
+//int fib(int n) {
+//	cnt++;
+//	if (n == 0)
+//		return 1;
+//	else if (n == 1)
+//		return 2;
+//	else
+//		return fib(n - 1) + fib(n - 2);
+//}
+//
+//void main()
+//{
+//	fib(8);
+//	printf("%d", cnt);
+//}
+//int main()
+//{
+//	//int x = 1;
+//	//do {
+//	//	printf("%2d\n", x++);
+//	//} while (x--);
+//
+//	//int i = 10;
+//	//int j = 20;
+//	//int k = 3;
+//	//k *= i + j;
+//	////k *= (i + j);
+//
+//	//printf("%d\n", k);
+//
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	//hehe
+//	int /*hehe*/a = 10; int b = 20;
+//	printf("a = %d\n", a);
+//
+//	return 0;
+//}
+
+
+//
+//int a = 1;
+//
+//void test() {
+//    int a = 2;
+//    a += 1;
+//}
+//
+//int main() {
+//
+//    test();
+//    printf("%d\n", a);
+//    return 0;
+//}
+//
+//
+//int main()
+//{
+//	//int a = 0, c = 0;
+//
+//	//do {
+//	//	--c;
+//	//	a = a - 1;
+//	//} while (a > 0);
+//	//
+//	//int x = 0;
+//	//int y = 0;
+//
+//	//for (x = 0, y = 0; (y = 123) && (x < 4); x++)
+//	//	;
+//
+//	int year = 1009, * p = &year;
+//
+//
+//	//int year = 1009;
+//	//int* p = &year;
+//
+//	return 0;
+//}
+//
+//void reverse(char* left, char* right)
+//{
+//    while (left < right)
+//    {
+//        char tmp = 0;
+//        tmp = *left;
+//        *left = *right;
+//        *right = tmp;
+//
+//        left++;
+//        right--;
+//    }
+//}
+//
+//int main()
+//{
+//    char arr[100] = { 0 };
+//    gets(arr);
+//    int len = strlen(arr);
+//    reverse(arr, arr+len-1);
+//    printf("%s\n", arr);
+//    return 0;
+//}
+//
