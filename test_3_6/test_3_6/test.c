2281d71b7ced7a2e36ff59cd44b9c9e13a82e9d5
Janssen000908 <2601566046@qq.com>
2023-03-06 23:59:18 +0800

    cs

---
+++ test_3_6/test_3_6/test.c
@@ -0,0 +1,44 @@
+#define   _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+//int main()
+//{
+//	unsigned long pulArray[] = { 6,7,8,9,10 };
+//	unsigned long* pulPtr;
+//	pulPtr = pulArray;
+//	*(pulPtr + 3) += 3;
+//	printf("%d, %d\n", *pulPtr, *(pulPtr + 3));
+//
+//	return 0;
+//}
+//
+
+//
+//struct stu
+//{
+//    int num;
+//    char name[10];
+//    int age;
+//};
+//void fun(struct stu* p)
+//{
+//    printf("%s\n", (*p).name);
+//    return;
+//}
+//int main()
+//{
+//    struct stu students[3] = { {9801,"zhang",20},
+//                              {9802,"wang",19},
+//                              {9803,"zhao",18}
+//    };
+//    fun(students + 1);
+//    return 0;
+//}
+
+//#include < stdio.h >
+//struct S
+//{
+//	int a;
+//	int b;
+//};
