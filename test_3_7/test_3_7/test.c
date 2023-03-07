850f44d42bfd71f4fbe77c0a301434f88609f0bc
Janssen000908 <2601566046@qq.com>
2023-03-07 22:52:51 +0800

    了

---
+++ test_3_7/test_3_7/test.c
@@ -0,0 +1,56 @@
+#define   _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+//int main()
+//{
+//
+//	return 0;
+//}
+//int main()
+//{
+//	char arr[20] = "abcd";
+//	strcat(arr, arr);//?
+//	printf("%s\n", arr);
+//	return 0;
+////}
+//
+//int main()
+//{
+//	/*char* p = "obc";
+//	char* q = "abcdef";
+//	if (p > q)
+//	{
+//		printf(">\n");
+//	}
+//	else
+//	{
+//		printf("<=\n");
+//	}*/
+//
+//	//if ("obc" > "abcdef")
+//	//{
+//
+//	//}
+
+//int my_strcmp(const char* s1, const char* s2)
+//{
+//	assert(s1 && s2);
+//	while (*s1 == *s2)
+//	{
+//		if (*s1 == '\0')
+//		{
+//			return 0;
+//		}
+//		s1++;
+//		s2++;
+//	}
+//	if (*s1 > *s2)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return -1;
+//	}
+//}
