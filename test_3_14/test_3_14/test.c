43916e0b087aff2f17692ee07da8ba73f1005844
Janssen000908 <2601566046@qq.com>
2023-03-14 22:27:49 +0800

    测试提交14

---
+++ test_3_14/test_3_14/test.c
@@ -0,0 +1,75 @@
+#define   _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+//void print(int* str, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", *(str + i));
+//	}
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	print(arr, sz);
+//	return 0;
+//}
+
+//void test(int** str)
+//{
+//	**str = 5;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;
+//	int*(* ppa) = &pa;
+//
+//	test(ppa);
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//º¯ÊýÖ¸Õë
+
+
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//
+//int main()
+//{
+//	/*int a = 10;
+//	int* pa = &a;
+//	char c = 'w';
+//	char* ch = &c;
+//	int  arr[10] = { 0 };
+//	int(* parr)[10] = arr;*/
+//
+//	int (*pf)(int,int) = &Add;
+//	printf("%p\n", &Add);
+//
+//
+//	return 0;
+//}
+//void test(char* str)
+//{
+//
+//}
+//int main()
+//{
+//	void (*pt)(char*) = &test;
+//	return 0;
+//}
+
+
+//int main()
+//{
+//
+//	return 0;
+//}
