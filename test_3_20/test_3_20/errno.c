9d72786abb172c49fc69c6de64fbf6c7be381ef5
Janssen000908 <2601566046@qq.com>
2023-03-20 11:26:32 +0800

    20

---
+++ test_3_20/test_3_20/errno.c
@@ -0,0 +1,24 @@
+#define   _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include<string.h>
+#include <errno.h>
+
+int main()
+{
+	//printf("%s\n", strerror(0));
+	//printf("%s\n", strerror(1));
+	//printf("%s\n", strerror(2));
+	//printf("%s\n", strerror(3));
+	//printf("%s\n", strerror(4));
+
+	FILE* pf = fopen("test.txt","r");
+	if (pf == NULL)
+	{
+		printf("%s\n", strerror(errno));
+		return 1;
+	}
+	fclose(pf);
+	pf = NULL;
+	return 0;
+}
