ede375f932f2da361eaf5dc8c78f1f873123992e
Janssen000908 <2601566046@qq.com>
2023-03-20 22:29:16 +0800

    20

---
+++ test_3_20(struct)/test_3_20(struct)/struct.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <time.h>
+#include <errno.h>	
---
+++ test_3_20(struct)/test_3_20(struct)/test2.c
@@ -0,0 +1,8 @@
+#define   _CRT_SECURE_NO_WARNINGS
+#include "struct.h"
+
+int main()
+{
+
+	return 0;
+}
